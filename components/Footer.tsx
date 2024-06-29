fc9892295ddd3439c8497c2420aeea616cf063fd
Gloria Hornero <snootchie.boochies@gmail.com>
2024-06-29 20:29:26 +0200

    draft layout

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer
+      className={"bg-blue-400 h-10 text-xs text-white text-center flex items-center justify-center"}
+    >
+      Powered by Glo
+    </footer>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <header
+            className={
+                "h-12 bg-pink-500 text-white flex flex-row items-center px-4"
+            }
+        >
+            <div className="font-extrabold text-xl text-white mr-4">
+                Recipes
+            </div>
+
+        </header>
+    );
+}
