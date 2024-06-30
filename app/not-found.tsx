9d1e7a9cd717180ed4858013b63c4c50cd012290
Gloria Hornero <snootchie.boochies@gmail.com>
2024-06-30 19:52:39 +0200

    adds not found pages

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <div className="m-40 flex flex-col justify-center items-center">
+        <header className="font-extrabold text-7xl">Not Found</header>
+        <p className="m-4 text-5xl">Sorry... we cannot find the resource :(</p>
+      </div>
+    )
+}
