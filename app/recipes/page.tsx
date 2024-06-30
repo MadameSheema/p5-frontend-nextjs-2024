bf0cd43ba4dff2acd640d30f6b891ea555959265
Gloria Hornero <snootchie.boochies@gmail.com>
2024-06-30 10:06:06 +0200

    adding an extra recipe + playing with the recipes page

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -2,7 +2,15 @@ import { getRecipes } from "@/lib/recipes"
 
 export default async function page() {
     const recipes = await getRecipes()
+
     return (
-      <main>{recipes[0].title}</main>
-    )
-  }
+        <main>
+            {recipes.map((recipe) => (
+                <div className="flex flex-col">
+                    <img key={recipe.id} src={recipe.image} alt={recipe.title} />
+                    {recipe.title}
+                </div>
+            ))}
+        </main>
+    );
+}
