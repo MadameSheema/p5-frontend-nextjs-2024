eae9796a0c547bb8e3c0933f63bf625ae9e8106e
Gloria Hornero <snootchie.boochies@gmail.com>
2024-06-30 10:56:11 +0200

    improves the recipes page by adding shadcn

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,16 +1,26 @@
 import { getRecipes } from "@/lib/recipes"
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
 export default async function page() {
     const recipes = await getRecipes()
 
     return (
-        <main>
-            {recipes.map((recipe) => (
-                <div className="flex flex-col">
-                    <img key={recipe.id} src={recipe.image} alt={recipe.title} />
-                    {recipe.title}
-                </div>
-            ))}
-        </main>
+            <main className="flex flex-row m-4 flex-wrap space-x-4">
+                {recipes.map((recipe) => (
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>{recipe.title}</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <img className="rounded" key={recipe.id} src={recipe.image} alt={recipe.title} />
+                        </CardContent>
+                    </Card>
+                ))}
+            </main>
     );
 }
